151453827f885e64a2b212b8d9cc58d6810e754d
ART <bykittipong@gmail.com>
2022-05-23 21:03:16 +0700

    update from finish

--- homework/homework/settings.py
+++ homework/homework/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hwapp'
+    'hwapp',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static" , 
+    r'C:\Art\sendHomeWork\summers\ProjectDjango\homework',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- homework/hwapp/views.py
+++ homework/hwapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
 # Create your views here.
+def Home(request):
+    return render(request, 'sent/home.html')
+    
+
+
+def Contects(request):
+    return render(request, 'sent/contect.html')
+
+
+def Login(request):
+    return render(request, 'sent/login.html')
+
+
+def Register(request):
+    return render(request, 'sent/register.html')
